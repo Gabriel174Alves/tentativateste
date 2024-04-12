dfeb0c0e1a1d72fe551f9be43be0ca742c60f58d
Gabriel Alves Coelho <gaa.alves74@gmail.com>
2024-04-12 19:45:25 -0300

    prep para render

--- gabriel/Scripts/activate.bat
+++ gabriel/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Gabriel\Downloads\codigo_final\gabriel
+set VIRTUAL_ENV=C:\Users\Gabriel\Desktop\tentativateste\gabriel
 
 if not defined PROMPT set PROMPT=$P$G
 
